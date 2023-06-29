e3941c1850b77f4d21c256f2a665b94de536f681
Wolleys <wolleysmigaya@gmail.com>
2023-06-29 14:03:23 +0000

    A class named EVCar that extends the Car class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
+
+export default EVCar
