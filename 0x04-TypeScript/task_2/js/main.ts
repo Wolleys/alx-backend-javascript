886feb562e7234782b698558db97f0310b30380c
Wolleys <wolleysmigaya@gmail.com>
2023-07-05 12:28:30 +0000

    String literal Subjects type and teachClass function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,15 @@ function executeWork(employee: DirectorInterface | TeacherInterface): string {
     return employee.workTeacherTasks();
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return 'Invalid class';
+  }
+}
