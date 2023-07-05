70db93b2999cc1c1211e3b6d52ae4c49810672d8
Wolleys <wolleysmigaya@gmail.com>
2023-07-05 15:00:39 +0000

    Add documentation for the student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,9 +1,11 @@
-// Define the Student interface
+/**
+ * Represents the information of a student.
+ */
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+	firstName: string; // The first name of the student.
+	lastName: string; // The last name of the student.
+	age: number; // The age of the student.
+	location: string; // The location of the student.
 }
 
 // Create two student objects
@@ -12,26 +14,35 @@ const firstStudent: Student = {
 	lastName: "Doe",
 	age: 26,
 	location: "Germany"
-}
+};
 
 const secondStudent: Student = {
 	firstName: "Jane",
 	lastName: "Doe",
 	age: 22,
 	location: "England"
-}
+};
 
 // Create an array of students
 const studentsList = [firstStudent, secondStudent];
 
 const table = document.getElementById('studentTable') as HTMLTableElement;
 
-// Create a new row for each student and append it to the table
-studentsList.forEach((student) => {
-	const row = table.insertRow();
-	const firstNameCell = row.insertCell(0);
-	const locationCell = row.insertCell(1);
+/**
+ * Renders the student information in a table.
+ * @param students - An array of student objects.
+ */
+function renderStudentsTable(students: Student[]): void {
+	students.forEach((student) => {
+		const row = table.insertRow();
+		const firstNameCell = row.insertCell(0);
+		const locationCell = row.insertCell(1);
 
-	firstNameCell.innerHTML = student.firstName;
-	locationCell.innerHTML = student.location;
-})
+		firstNameCell.innerHTML = student.firstName;
+		locationCell.innerHTML = student.location;
+	});
+}
+
+// Render the students table
+renderStudentsTable(studentsList);
+
