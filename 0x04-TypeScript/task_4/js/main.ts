5c627ceacf73c073e463fcbe34ea148c47a027ea
Wolleys <wolleysmigaya@gmail.com>
2023-07-05 14:25:21 +0000

    Create and export constants

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,30 @@
+import { Subjects } from './subjects/Subject';
+import { Cpp } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
+
+export const cpp: Subjects.Cpp = new Cpp();
+export const java: Subjects.Java = new Java();
+export const react: Subjects.React = new React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
