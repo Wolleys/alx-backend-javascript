b876b540e37b7e8dfe5b4d54dece7c9f9eeb84c0
Wolleys <wolleysmigaya@gmail.com>
2023-07-05 10:32:09 +0000

    An interface for printTeacherFunction function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,14 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${firstLetter}. ${formattedLastName}`;
+};
+
