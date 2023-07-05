b678e66877d0463c182a5b8c9c458a4efc7b38ca
Wolleys <wolleysmigaya@gmail.com>
2023-07-05 12:11:27 +0000

    Create isDirector and executeWork functions

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,3 +45,15 @@ function createEmployee(salary: number | string): DirectorInterface | TeacherInt
     return new Director();
   }
 }
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
